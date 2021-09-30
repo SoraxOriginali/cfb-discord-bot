470f1134599157a7235e7cd6ece4cab1bc57d06c
Colin Kraebel <ckraebel@steadyapp.com>
2021-09-30 11:42:10 -0400

    feat: add DSharpPlus dependencies and ping slash command
    Refs #1, #3

--- cfb-discord-bot/Worker.cs
+++ cfb-discord-bot/Worker.cs
@@ -1,21 +1,56 @@
-namespace cfb_discord_bot
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace CFBDiscordBot;
+
+public class Worker : BackgroundService
 {
-    public class Worker : BackgroundService
+    private readonly ILogger<Worker> _logger;
+    private readonly IConfiguration _configuration;
+    private DiscordClient _discordClient;
+
+    public Worker(ILogger<Worker> logger, IConfiguration configuration)
     {
-        private readonly ILogger<Worker> _logger;
+        _logger = logger;
+        _configuration = configuration;
 
-        public Worker(ILogger<Worker> logger)
+        string discordBotToken = _configuration["DiscordBotToken"];
+        _discordClient = new DiscordClient(new DiscordConfiguration()
         {
-            _logger = logger;
-        }
+            Token = discordBotToken,
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.AllUnprivileged
+        });
+    }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
-        }
+    public override async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Starting discord bot");
+
+        var soraxLtdGuildId = Convert.ToUInt64(_configuration["SoraxLtdGuildId"]);
+        _discordClient.UseSlashCommands()
+            .RegisterCommands<SlashCommands>(soraxLtdGuildId);
+
+        await _discordClient.ConnectAsync();
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken) => Task.CompletedTask;
+
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _discordClient.DisconnectAsync();
+        _discordClient.Dispose();
+        _logger.LogInformation("Discord bot stopped");
+    }
+}
+
+public class SlashCommands : ApplicationCommandModule
+{
+    [SlashCommand("ping", "The default pong response.")]
+    public async Task Ping(InteractionContext ctx)
+    {
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+            .WithContent("Pong!"));
     }
 }
